be5479a432aec6587529f7240b035868f73675ec
PavelSynek <pavel.synek@gmail.com>
2016-11-19 15:04:25 +0100

    updated selector

--- kb.py
+++ kb.py
@@ -4,31 +4,22 @@ import requests
 import lxml.html as lh
 
 
-
 url = "https://www.kb.cz"
 
 r = requests.get('https://www.kb.cz/cs/transparentni-ucty/')
 
-doc = lh.fromstring(r.text.encode('ISO-8859-1'))
+doc = lh.fromstring(r.text)
 
 
 for div in doc.cssselect('#content > div.listing-object > table > tbody > tr'):
     url_account = url + div.cssselect('td.ar > a')[0].get('href')
     first_page = requests.get(url_account)
 
-    print(first_page.encoding)
-
     doc2 = lh.fromstring(first_page.text.encode('ISO-8859-1'))
-
     for div2 in doc2.cssselect('#content > div.listing-object > table > tbody > tr'):
-        for div3 in div2.cssselect('td > table > tbody > tr > td'):
+        for div3 in div2.cssselect('tr > td'):
             print(div3.text)
-#        print(div2.cssselect('td:nth-child(2) > table > tbody > tr:nth-child(1) > td')[0].text.encode('utf-8'))
-
-#content > div.listing-object > table > tbody > tr:nth-child(5) > td:nth-child(2) > table > tbody > tr.breakable > td
-#content > div.listing-object > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > td
-#content > div.listing-object > table.transaction-table.stack-table.stack-table-sm.listing-table.large-only.small-only > tbody > tr:nth-child(19) > td > table > tbody > tr:nth-child(2) > td
-#content > div.listing-object > table.transaction-table.stack-table.stack-table-sm.listing-table.large-only.small-only > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(3) > td
+        print("################################")
 """
     print(first_page.text.encode('utf-8'))
 
