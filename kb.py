c9ca25b525d6ae61cda76be39ea7b946e6a441c1
Paffilda <pavlina.fronkova@gmail.com>
2016-11-19 15:06:15 +0100

    major changes

--- kb.py
+++ kb.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
-
 import requests
 import lxml.html as lh
-
 
 
 url = "https://www.kb.cz"
@@ -16,25 +13,23 @@ for div in doc.cssselect('#content > div.listing-object > table > tbody > tr'):
     url_account = url + div.cssselect('td.ar > a')[0].get('href')
     first_page = requests.get(url_account)
 
-    print(first_page.encoding)
+    #print(first_page.encoding)
 
     doc2 = lh.fromstring(first_page.text.encode('ISO-8859-1'))
 
     for div2 in doc2.cssselect('#content > div.listing-object > table > tbody > tr'):
         for div3 in div2.cssselect('td > table > tbody > tr > td'):
+            #print('firstpage')
             print(div3.text)
 #        print(div2.cssselect('td:nth-child(2) > table > tbody > tr:nth-child(1) > td')[0].text.encode('utf-8'))
 
-#content > div.listing-object > table > tbody > tr:nth-child(5) > td:nth-child(2) > table > tbody > tr.breakable > td
-#content > div.listing-object > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > td
-#content > div.listing-object > table.transaction-table.stack-table.stack-table-sm.listing-table.large-only.small-only > tbody > tr:nth-child(19) > td > table > tbody > tr:nth-child(2) > td
-#content > div.listing-object > table.transaction-table.stack-table.stack-table-sm.listing-table.large-only.small-only > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(3) > td
-"""
-    print(first_page.text.encode('utf-8'))
-
     x = 1
+
     while requests.get(url_account[:-6]+"-"+str(x)+".shtml").status_code == 200:
-        print(x)
+        for div2 in doc2.cssselect('#content > div.listing-object > table > tbody > tr'):
+            for div3 in div2.cssselect('td > table > tbody > tr > td'):
+                print(div3.text)
+        #print('nextpage {}'.format(x))
         x += 1
 
 
@@ -44,4 +39,3 @@ for div in doc.cssselect('#content > div.listing-object > table > tbody > tr'):
     #    f.write(str(s.text.encode('utf-8')))
 
 #content > div.pagination-wrapper > ul > li:nth-child(6)
-"""
